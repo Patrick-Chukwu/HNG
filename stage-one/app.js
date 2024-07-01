e7f03c6ddbf41297518adfa3b85bd12b968cfcce
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-01 15:46:42 +0100

    Ensure the time updates

--- stage-one/app.js
+++ stage-one/app.js
@@ -1,15 +1,37 @@
-const currentTime = document.getElementById("time");
+const updateTime = () => {
+    const currentTime = document.getElementById("time");
 const currentDay = document.getElementById("day");
 
 // use the date object
 const currentCompleteTime = new Date();
 
 // for UTC time
-const time = currentCompleteTime.toUTCString();
-currentTime.textContent = time;
+let hour = currentCompleteTime.getUTCHours();
+const minutes = currentCompleteTime.getUTCMinutes();
+const seconds = currentCompleteTime.getUTCSeconds();
+
+// change to WAT
+const watHour = (hour + 1) % 24;
+
+// format time
+const formattedHour = String(watHour).padStart(2,"0");
+const formattedMinutes = String(minutes).padStart(2, "0");
+const formattedSeconds = String(seconds).padStart(2, "0");
+
+const time = `${formattedHour}:${formattedMinutes}:${formattedSeconds}`;
+// console.log(time)
+currentTime.textContent = "Current time is:" + time;
+
 
 // for day
 const day = currentCompleteTime.getDay();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const today = days[day];
+// console.log(today)
 currentDay.textContent = "Today is: " + today ;
+
+
+}
+
+setInterval(updateTime, 1000)
+updateTime()
